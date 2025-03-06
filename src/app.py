8e8d1b0fcede5c8a3168a549f58c0aea27684851
Fernando <fernando.decalle@gmail.com>
2025-03-06 20:56:03 +0100

    with get

--- src/app.py
+++ src/app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
 
-# from pymongo import MongoClient
-# client = MongoClient("mongodb://root:example@mongo:27017")
-# db = client['mi_collccion']
-# col = db['test_col']
-# col.insert_one(
-#     {'name':'test', 'sadg': 2, 'sg': 3}
-# )
+
+client = MongoClient("mongodb://root:example@mongo:27017")
+
+db = client['db_api']
+col = db['nombres']
+
 
 @app.get("/names")
 def names():
@@ -17,7 +17,13 @@ def names():
 
 
 @app.post("/name")
-def name(name: str):
+def name(
+    name: str,
+    phone: int,
+):
     print(f'call with {name}')
+    col.insert_one(
+        {'name': name, 'phone': phone}
+    )
     return {'name': name}
 
