3dbeab9fa2f79b20ab246725447a0aa0bce71aad
Fernando <fernando.decalle@gmail.com>
2025-03-06 21:11:54 +0100

    depends for db

--- src/app.py
+++ src/app.py
@@ -1,19 +1,21 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pymongo import MongoClient
 
 app = FastAPI()
 
 
-client = MongoClient(os.getenv('MONGO_CNX'))
-
-db = client['db_api']
-col = db['nombres']
+def get_collection():
+    client = MongoClient(os.environ['MONGO_CNX'])
+    db = client['db_api']
+    return db['nombres']
 
 
 @app.get("/names")
-def names():
+def names(
+    col=Depends(get_collection)
+):
     names_list = col.find({}, {"_id": 0, "name": 1})
     return [name['name'] for name in names_list]
 
@@ -22,10 +24,10 @@ def names():
 def name(
     name: str,
     phone: int,
+    col=Depends(get_collection),
 ):
     print(f'call with {name}')
     col.insert_one(
         {'name': name, 'phone': phone}
     )
     return {'name': name}
-
