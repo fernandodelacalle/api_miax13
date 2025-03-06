22229a646c6e3612c7fa3e466c3850e279782b48
Fernando <fernando.decalle@gmail.com>
2025-03-06 20:25:27 +0100

    with docker

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ uvicorn app:app
 ```
 
 En el navegador: http://127.0.0.1:8000/docs
+
+
+# Con docker
+```bash
+docker-compose build
+```
+
+```bash
+docker-compose up
+```
+
+En el navegador: http://localhost:8080/docs
--- src/app.py
+++ src/app.py
@@ -5,10 +5,12 @@ app = FastAPI()
 
 @app.get("/names")
 def names():
-    return ['fer', 'jose', 'juan', 'pedro', 'luis', 'maria', 'ana']
+    print('call names')
+    return ['fernando', 'jose', 'juan', 'pedro', 'luis', 'maria', 'ana']
 
 
 @app.post("/name")
 def name(name: str):
+    print(f'call with {name}')
     return {'name': name}
 
